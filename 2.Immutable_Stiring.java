a531af16732b0c6a06c32969967abd7f40a3f5ba
nithinlambu <124145788+nithinlambu@users.noreply.github.com>
2023-02-08 00:00:30 +0530

    Add files via upload

---
+++ 2.Immutable_Stiring.java
@@ -0,0 +1,12 @@
+//G.Raveendra
+//Div : 4b19
+//ERP:210303126116
+
+
+class Testimmutablestring1{  
+     public static void main(String args[]){  
+       String s="Sachin";  
+       s=s.concat(" Tendulkar");  
+       System.out.println(s);  
+     }  
+    }  
